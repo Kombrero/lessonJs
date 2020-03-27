2c6938713335d8ce51153d0297679d23d493a5ab
Slava Kovalev <reqwiem96@mail.ru>
2020-03-27 18:39:01 +0300

    Добавил значения переменным вывел часть логов

--- script/script.js
+++ script/script.js
@@ -6,9 +6,13 @@ deposit = true,
 mission = 400000,
 period = 6;
 
-alert ('Не знаю что написать!');
+//alert ('Не знаю что написать!');
 
 // Выводы в консоль
 console.log(money);
 console.log(income);
 console.log(deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей' );
+console.log(addExpenses.toLowerCase().split(', '));
