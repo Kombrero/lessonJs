5a672b8acf6afd12511764b73c13f9852e4ea860
Slava Kovalev <reqwiem96@mail.ru>
2020-03-27 18:00:34 +0300

    Добавил значение в переменные и вывод в консоль переменных

--- script/script.js
+++ script/script.js
@@ -1,10 +1,14 @@
-let money,
-income,
-addExpenses,
-deposit,
-mission,
-period;
+// Переменные
+let money = 30000,
+income = 10000,
+addExpenses = 'Интернет, бензин, продукты',
+deposit = true,
+mission = 400000,
+period = 6;
 
 alert ('Не знаю что написать!');
 
-console.log('Поэтому здесь будет удивленный смайик (о_о)');
+// Выводы в консоль
+console.log(money);
+console.log(income);
+console.log(deposit);
