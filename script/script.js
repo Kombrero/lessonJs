db51b82878877f14e578db0334116267018de26b
Slava Kovalev <reqwiem96@mail.ru>
2020-03-27 18:48:36 +0300

    Вывел тип данных у переменных, дописал все логи

--- script/script.js
+++ script/script.js
@@ -4,15 +4,17 @@ income = 10000,
 addExpenses = 'Интернет, бензин, продукты',
 deposit = true,
 mission = 400000,
-period = 6;
+period = 6,
+budgetDay = money/30;
 
 //alert ('Не знаю что написать!');
 
 // Выводы в консоль
-console.log(money);
-console.log(income);
-console.log(deposit);
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
 console.log(addExpenses.length);
 console.log('Период равен ' + period + ' месяцев');
 console.log('Цель заработать ' + mission + ' рублей' );
 console.log(addExpenses.toLowerCase().split(', '));
+console.log(budgetDay);
